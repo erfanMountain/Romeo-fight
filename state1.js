c84b361df26cc763ddb4b322394a36a4e210c893
erfan <ahmederfan822@gmail.com>
2021-03-18 11:53:48 +0200

    adding the changes for hitting the layers

--- state1.js
+++ state1.js
@@ -1,4 +1,7 @@
 demo.state1 = function(){};
+
+var moves, velocit = 500, rocks, grass;
+
 demo.state1.prototype = {
 	preload: function(){
 		game.load.tilemap('field', 'assets/tilemaps/field.json',
@@ -17,12 +20,43 @@ demo.state1.prototype = {
 		var map = game.add.tilemap('field');
 		map.addTilesetImage('grassTiles');
 		map.addTilesetImage('rockTiles');
-		var grass = map.createLayer('grass');
-		var  rock = map.createLayer('rocks');
+		grass = map.createLayer('grass');
+		rocks = map.createLayer('rocks');
 
 		romeo = game.add.sprite(200, 200, 'Romeo');
 		romeo.scale.setTo(0.2,0.2);
+		game.physics.enable(romeo);
 
+
+		map.setCollisionBetween(1, 9, true, 'rocks'); // setting the collision with sprite according to json file, index 1-9 repesent rock tiles, 10 index represent normal grass type and 11 represent the darker grass type
+		map.setCollision(11, true, 'grass'); // It will hit darkish grass layer
+		moves = game.input.keyboard.createCursorKeys();
 	},
-	update: function(){}
+	/* Inside the update function we are using the logic for moving the sprite inside the tiled
+	map
+	*/
+	update: function(){
+		game.physics.arcade.collide(romeo, rocks, function (){console.log('Romeo is hitting the  rocks:');}); // here we are using the annonymys function
+		game.physics.arcade.collide(romeo, grass, function (){console.log('Romeo is hitting the  grass:');}); // here we are using the annonymys function
+
+		if (moves.up.isDown) {
+			romeo.body.velocity.y = -velocit; // we are using the velocity for coliding sprite with the rock
+		}
+		else if(moves.down.isDown) {
+			romeo.body.velocity.y = velocit; // we are using the velocity for coliding with the rock
+		}
+		else {
+			romeo.body.velocity.y = 0;
+		}
+		if(moves.left.isDown) {
+			romeo.body.velocity.x = -velocit; // we are using the velocity for coliding with the rock
+		}
+		else if(moves.right.isDown) {
+			romeo.body.velocity.x = velocit; // we are using the velocity for coliding with the rock
+		}
+		else {
+			romeo.body.velocity.x = 0;
+		}
+
+	}
 };
