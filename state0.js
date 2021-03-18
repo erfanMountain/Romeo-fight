432fd24c5b125ad9f2cb264e10f398fb39f51622
erfan <ahmederfan822@gmail.com>
2021-03-18 11:01:35 +0200

    Adding the changes for state 0 and 1

--- state0.js
+++ state0.js
@@ -49,6 +49,7 @@ demo.state0.prototype = {
 };
 
 function changeState(i, stateNum){
+    console.log('state' + stateNum);
     game.state.start('state' + stateNum);  
 }
 
@@ -62,9 +63,4 @@ function addChangeStateEventListeners(){
         addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
         addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
         addKeyCallback(Phaser.Keyboard.FOUR, changeState, 4);
-        addKeyCallback(Phaser.Keyboard.FIVE, changeState, 5);
-        addKeyCallback(Phaser.Keyboard.SIX, changeState, 6);
-        addKeyCallback(Phaser.Keyboard.SEVEN, changeState, 7);
-        addKeyCallback(Phaser.Keyboard.EIGHT, changeState, 8);
-        addKeyCallback(Phaser.Keyboard.NINE, changeState, 9);
 }
--- state1.js
+++ state1.js
@@ -1,27 +1,28 @@
 demo.state1 = function(){};
 demo.state1.prototype = {
-preload: function(){
-	/*
-The code below will change
-the heading with id = "myH"
-and the paragraph with id = "myP"
-in my web page:
-*/
-		game.load.tilemap('grass', 'assets/maps/grass.json',
-		null, Phaser.Tilemap.TILED_JSON);
-		game.load.image('gTiles', 'assets/maps/gTiles.png');
-		game.load.image('rTiles', 'assets/maps/rTiles.png');
-		},
+	preload: function(){
+		game.load.tilemap('field', 'assets/tilemaps/field.json',
+			null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('grassTiles', 'assets/tilemaps/grassTiles.png');
+		game.load.image('rockTiles', 'assets/tilemaps/rockTiles.png');
+		game.load.image('Romeo', 'assets/sprites/Romeo.png');
+	},
 	create: function(){
+		game.physics.startSystem(Phaser.Physics.Arcade)
 		game.state.backgroundColor = '#cc99ff';
-        //game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
-        //game.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(changeState, null, null, 0); 
-        addChangeStateEventListeners();
-        var map = game.add.tilemap('grass');
-        map.addTilesetImage('gTiles');
-        map.addTilesetImage('rTiles');
-        var rock = map.createLayer('rock');
-        var  Tile = map.createLayer('Tile Layer 1');
+		console.log('state1');
+		//game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
+		//game.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(changeState, null, null, 0);
+		addChangeStateEventListeners();
+		var map = game.add.tilemap('field');
+		map.addTilesetImage('grassTiles');
+		map.addTilesetImage('rockTiles');
+		var grass = map.createLayer('grass');
+		var  rock = map.createLayer('rocks');
+
+		romeo = game.add.sprite(200, 200, 'Romeo');
+		romeo.scale.setTo(0.2,0.2);
+
 	},
 	update: function(){}
 };
