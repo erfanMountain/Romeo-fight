31d7a58aedca108b5fe5510f111bd0f972124871
erfan <ahmederfan822@gmail.com>
2021-03-19 08:30:46 +0200

    Adding the changes for stage 3 and stage 4

--- state3.js
+++ state3.js
@@ -2,7 +2,7 @@ demo.state3 = function(){};
 demo.state3.prototype = {
 	preload: function(){},
 	create: function(){
-		game.state.backgroundColor = '#ffff66';
+		game.stage.backgroundColor = '#ffff66';
         console.log('state3');
         addChangeStateEventListeners();
 	},
--- state4.js
+++ state4.js
@@ -2,7 +2,7 @@ demo.state4 = function(){};
 demo.state4.prototype = {
 	preload: function(){},
 	create: function(){
-		game.state.backgroundColor = '#ff6600';
+		game.stage.backgroundColor = '#ff6600';
         console.log('state4');
         addChangeStateEventListeners();
 	},
